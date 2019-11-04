eb1758081063475c0123fb83d92775794f44fe53
Atilla Ozgur <ati.ozgur@gmail.com>
2019-11-04 09:56:40 +0100

    solutions added

--- Labs/Lab05/lucky_digit_solution.py
+++ Labs/Lab05/lucky_digit_solution.py
@@ -3,16 +3,16 @@
 # entered_number = 190837523478
 
 # total = 1+9+0+8+3+7+5+2+3+4+7+8#  = 57
-
-def find_total(number, total = 0):
+# Change this code so that it will find your lucky digit
+def find_lucky_number(number, total = 0):
     if (number > 10):
         total = total + number % 10
         number = number // 10
-        find_total(number,total)
+        find_lucky_number(number,total)
     # Exit condition 
     else:
         total = total + number
         print("total is ", total)
 
-find_total(19834344343)
+find_lucky_number(1987)
 
