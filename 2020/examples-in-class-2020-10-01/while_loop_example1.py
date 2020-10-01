c6180e10d8c4015816656784daf524bc7a55be77
Atilla Ozgur <ati.ozgur@gmail.com>
2020-10-01 12:07:20 +0200

    while loop examples are added

---
+++ 2020/examples-in-class-2020-10-01/while_loop_example1.py
@@ -0,0 +1,6 @@
+
+n = 10
+i = 0
+while i < n:
+	print(i)
+	i = i + 1 
---
+++ 2020/examples-in-class-2020-10-01/while_loop_example_three_five_fifteen.py
@@ -0,0 +1,14 @@
+
+index = 1
+while index <= 100:
+
+	if index % 15 == 0:
+		print("fifteen")
+	elif index % 3 == 0:
+		print("three")
+	elif index % 5 == 0:
+		print("five")
+	else:
+		print(index)
+
+	index = index + 1
