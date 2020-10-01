96c6790549332e19c4724e5467a0d566b0dee144
Atilla Ozgur <ati.ozgur@gmail.com>
2020-10-01 11:41:41 +0200

    answers added to assignment

---
+++ 2020/assignment02_answer_01.py
@@ -0,0 +1,6 @@
+# N = int(input("Please enter an integer N so that we will find summation of 1..N: "))
+# I like this 2 line  more.
+str_N = input("Please enter an integer N so that we will find summation of 1..N: ")
+N = int(str_N)
+print(f"summation of 1..N: ",sum(range(N+1)))
+
---
+++ 2020/assignment02_answer_02.py
@@ -0,0 +1,6 @@
+str_N = input("please enter a number between 1-10: ")
+N = int(str_N) 
+
+
+for index in range(N+1):
+	print("*"*index)
