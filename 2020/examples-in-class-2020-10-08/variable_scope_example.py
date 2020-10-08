b9c7c24ee6933d805592ad57e9407cff640c44e3
Atilla Ozgur <ati.ozgur@gmail.com>
2020-10-08 11:58:16 +0200

    example is added

---
+++ 2020/examples-in-class-2020-10-08/variable_scope_example.py
@@ -0,0 +1,20 @@
+var1 = 10
+
+
+def func_example1():
+	#print("    var1 inside func_example1 before assignment",var1)
+
+	print("    inside func_example1 starts")
+	var1 = 15
+	print("    var1 inside func_example1",var1)
+	print("    inside func_example1 ends")
+
+
+
+
+print("var1",var1)
+func_example1()
+print("var1",var1)
+
+
+
