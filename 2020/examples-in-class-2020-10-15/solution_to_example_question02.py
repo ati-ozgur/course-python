722db11f1a4b63d44ba5ead6e3ecf17f652221a3
Atilla Ozgur <ati.ozgur@gmail.com>
2020-10-15 11:46:35 +0200

    new example added

---
+++ 2020/examples-in-class-2020-10-15/solution_to_example_question02.py
@@ -0,0 +1,37 @@
+
+# Write palindrome function again but use a loop that goes for every character in the input string.
+
+# TODO instead of looping all, stop at half of the string
+def check_palindrome(str_to_test):
+		#reverse loop
+		index_reverse = len(str_to_test) -1
+		index_forward = 0
+		while index_reverse >= 0:
+			#print(index_reverse)
+			char_forward = str_to_test[index_forward]
+			char_reverse = str_to_test[index_reverse]
+			#print("index_forward",index_forward,"char_forward:",char_forward,"index_reverse",index_reverse,"char_reverse",char_reverse)
+			if char_forward != char_reverse:
+				print(f"{str_to_test} is not a palindrome ")
+				return
+			index_reverse = index_reverse - 1
+			index_forward = index_forward + 1
+
+
+
+		print(f"{str_to_test} is a palindrome ")
+
+
+
+
+
+check_palindrome("efe")
+check_palindrome("hannah")
+check_palindrome("ava")
+check_palindrome("anna")
+check_palindrome("nixon")
+check_palindrome( "example")
+check_palindrome( "xxxzz")
+check_palindrome("xxxzzxxx")
+
+
