c0d76c8b4e8926fa8adcdb864f45cce6629875a4
Atilla Ozgur <ati.ozgur@gmail.com>
2020-10-29 12:23:58 +0100

    examples added

---
+++ 2020/examples-in-class-2020-10-29/check_if_character_in_a_string.py
@@ -0,0 +1,4 @@
+print("a" in "Atilla")
+print("a" in "Atill")
+print("A" in "Atill")
+print("A" in "till")
--- 2020/examples-in-class-2020-10-29/example_complex_password.py
+++ 2020/examples-in-class-2020-10-29/example_complex_password.py
@@ -7,6 +7,8 @@
 # - At least contains 1 number
 # - At least contains 1 character from following special character list "!#$%&()*+,-./:;<=>?@[\]^_{|}"
 
+import string
+
 password = input("Please enter a complex password example:")
 
 
@@ -14,4 +16,15 @@ if len(password) <= 9:
 	print("your password should be at least 9 characters long")
 else:
 	print("Your password satisfies requirement of at least 9 characters long")
+
+any_lowercase_in_password = False
+for ch_lowercase in string.ascii_lowercase:
+	if ch_lowercase in password:
+		any_lowercase_in_password = True
+		break
+
+if any_lowercase_in_password:
+	print("Your password satisfies requirement of at least 1 lower case character")
+else:
+	print("your password should have at least 1 lower case character")
 
