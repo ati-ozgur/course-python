2fda1883ddda5e84af76ad76889e6ef6450d83c2
Atilla Ozgur <ati.ozgur@gmail.com>
2020-11-05 11:38:40 +0100

    new solutions

---
+++ 2020/examples-in-class-2020-11-05/example_pyramid.py
@@ -0,0 +1,19 @@
+def print_triangle(N):
+	for i in range(0,N+1):
+		index_reverse = N  - i
+		#line = " " * index_reverse 
+		line = "*" * (2*i+1) # + str(i)
+		print(line)
+
+	for i in range(N-1,-1,-1):
+		
+		index_reverse = N  - i
+		#line = " " * index_reverse + 
+		line = "*" * (2*i+1) # + str(i)
+		print(line)
+
+
+N = 5
+print_triangle(N)
+
+
