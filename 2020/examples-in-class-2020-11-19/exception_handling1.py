f75fdccc8ab433a8ebcb15b5d2bcfb28b2953e04
Atilla Ozgur <ati.ozgur@gmail.com>
2020-11-19 11:42:35 +0100

    example added

---
+++ 2020/examples-in-class-2020-11-19/exception_handling1.py
@@ -0,0 +1,12 @@
+
+result1 = 10 / 5
+print(f"result: {result1}")
+
+
+result2 = 10 / 0
+# Since we do not hand above error, below lines will not execute.
+print(f"result: {result2}")
+
+
+result3 = 10 / 2
+print(f"result: {result3}")
