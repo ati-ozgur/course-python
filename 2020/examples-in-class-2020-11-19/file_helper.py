ea9fcdc26c6d6c7cc47fb0fdf6a99ff7d2946b69
Atilla Ozgur <ati.ozgur@gmail.com>
2020-11-19 13:05:33 +0100

    examples are updated

--- 2020/examples-in-class-2020-11-19/file_helper.py
+++ 2020/examples-in-class-2020-11-19/file_helper.py
@@ -10,6 +10,26 @@ def print_files_in_directories_recursively(directory_name):
 		elif isdir(fullname):
 			print_files_in_directories_recursively(fullname)
 
+def print_files_in_directories_recursively2(directory_name,tab_count = 1):
+	for name in listdir(directory_name):
+		fullname = join(directory_name, name)
+		if isfile(fullname):
+			tabs = "    " * tab_count
+			line = f"{tabs}-{name}"
+			print(line)
+		elif isdir(fullname):
+			print_files_in_directories_recursively2(fullname,tab_count+1)
+
+def print_files_in_directories_recursively3(directory_name,tab_count = 1):
+	tabs = "    " * tab_count
+	print(tabs + directory_name)
+	for name in listdir(directory_name):
+		fullname = join(directory_name, name)
+		if isfile(fullname):
+			line = f"{tabs}-{name}"
+			print(line)
+		elif isdir(fullname):
+			print_files_in_directories_recursively3(fullname,tab_count+1)
 
 def print_files_in_directory(directory_name):
 	for file in listdir(directory_name):
