28857f5b271acf895c6f49f2f8dbb9e2c870c581
Atilla Ozgur <ati.ozgur@gmail.com>
2020-11-26 13:38:39 +0100

    v2

--- 2020/examples-in-class-2020-11-26/tree_command.py
+++ 2020/examples-in-class-2020-11-26/tree_command.py
@@ -3,12 +3,20 @@ from os.path import isfile,isdir, join
 
 def print_folders_in_directory(directory_name):
 	for name in listdir(directory_name):
+		# to skip hidden folders, like .git
+		if "." == name[0]:
+			continue
 		fullname = join(directory_name, name)
 		if isdir(fullname):
 			print(name)
+			print_folders_in_directory(fullname)
 
 
 #directory_name = "."
 
 
 print_folders_in_directory(".")
+
+
+#print_folders_in_directory("2020")
+#print_folders_in_directory("2020/examples-in-class-2020-11-19")
