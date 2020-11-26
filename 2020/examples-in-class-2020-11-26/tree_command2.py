25c2fc9325370ebf271a8ff3817c573b3783fa47
Atilla Ozgur <ati.ozgur@gmail.com>
2020-11-26 13:49:39 +0100

    example added

---
+++ 2020/examples-in-class-2020-11-26/tree_command2.py
@@ -0,0 +1,26 @@
+from os import listdir
+from os.path import isfile,isdir, join
+
+def print_folders_in_directory(directory_name,indentation_count = 1):
+	for name in listdir(directory_name):
+		# to skip hidden folders, like .git
+		if "." == name[0]:
+			continue
+		fullname = join(directory_name, name)
+		if isdir(fullname):
+			if indentation_count > 1:
+				line =  "____" * indentation_count   + name
+			else:
+				line = name
+			print(line)
+			print_folders_in_directory(fullname,indentation_count + 1)
+
+
+#directory_name = "."
+
+
+print_folders_in_directory(".")
+
+
+#print_folders_in_directory("2020")
+#print_folders_in_directory("2020/examples-in-class-2020-11-19")
