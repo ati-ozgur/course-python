33943adc28b3f985c38d7cbe600a0934d828269d
Atilla Ozgur <ati.ozgur@gmail.com>
2020-11-26 11:20:03 +0000

    new examples

---
+++ 2020/examples-in-class-2020-11-26/course2.py
@@ -0,0 +1,10 @@
+class Course:
+
+  def __init__(self,course_name,instructor_name):
+  	self.course_name = course_name
+  	self.instructor_name = instructor_name
+
+
+
+  def add_lesson_time(self,weekday,start_time):
+  	pass
