991e0ea864c1d7d3f1691fb316482fcc584b8b89
Atilla Ozgur <ati.ozgur@gmail.com>
2020-12-08 16:38:36 +0100

    update solution

--- course-content/questions/solutions/ceasar_cipher.py
+++ course-content/questions/solutions/ceasar_cipher.py
@@ -1,8 +1,9 @@
 def encode_in_ceaser_cipher(text):
 	encoded = ""
 	for ch in text:
-		v = ord(ch)
-		v = v + 3
+		v = ord(ch) - 65
+		v = (v + 3) % 26
+		v = v + 65
 		new_ch = chr(v)
 		encoded = encoded + new_ch
 	return encoded
@@ -11,9 +12,17 @@ def encode_in_ceaser_cipher(text):
 
 text = "ABC"
 encoded_text = encode_in_ceaser_cipher(text)
-print(encoded_text)
+print("text",text,"encoded_text", encoded_text)
 
 
 text = "XYZ"
 encoded_text = encode_in_ceaser_cipher(text)
-print(encoded_text)
+print("text",text,"encoded_text", encoded_text)
+
+text = "XYZ"
+encoded_text = encode_in_ceaser_cipher(text)
+print("text",text,"encoded_text", encoded_text)
+
+text = "ATILLAOZGUR"
+encoded_text = encode_in_ceaser_cipher(text)
+print("text",text,"encoded_text", encoded_text)
