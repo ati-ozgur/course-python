8f19daa2b1585d015d03d387d12384fee685f41c
Atilla Oezguer <ati.ozgur@gmail.com>
2022-09-16 14:13:13 +0200

    previous lesson information is added

---
+++ 2022/examples-in-class-2022-09-09/born_year.py
@@ -0,0 +1,4 @@
+print('please enter year of your birth date')
+year = input()
+print("you have born in " + year)
+
---
+++ 2022/examples-in-class-2022-09-09/hello_comments.py
@@ -0,0 +1,7 @@
+# 80 lines of code -> python -> output
+
+# 20 -> python -> output
+# 40
+# 60
+# 80
+print("hello world twice")
