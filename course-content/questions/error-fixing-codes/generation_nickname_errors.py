c3943379a817d08026feb101260f8ba1d4cd4bf0
Atilla Ozgur <ati.ozgur@gmail.com>
2023-11-01 20:35:35 +0100

    new questions are added

---
+++ course-content/questions/error-fixing-codes/generation_nickname_errors.py
@@ -0,0 +1,15 @@
+birth_year = input("please enter your birth year: ")
+
+if birth_year < 1946:
+    print("No nickname)
+elif birth_year >= 1946 and birth_year <= 1964:
+print("Baby Boomers")
+elif birth_year >= 1965 and birth_year <= 1980
+    print("Gen X")
+elif birth_year >= 1981 and birth_year <= 1996:
+print("Gen Y")
+elif birth_year >= 1997 and birth_year <= 2012:
+    print"Gen Z")
+elif birth_year > 2012:
+    print("Gen A"
+
---
+++ course-content/questions/errors-generation-nickname.md
@@ -0,0 +1,7 @@
+## errors generation nickname
+
+Please look at [generation_nickname_errors.py](error-fixing-codes/generation_nickname_errors.py). 
+Fix its errors so that it runs correctly.
+
+
+
