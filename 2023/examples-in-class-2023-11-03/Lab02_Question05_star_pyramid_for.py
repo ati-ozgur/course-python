74334ed7315162807a1faf79442002aee045cd08
Atilla Ozgur <ati.ozgur@gmail.com>
2023-11-03 13:08:34 +0100

    solutions v2

---
+++ 2023/examples-in-class-2023-11-03/Lab02_Question05_star_pyramid_for.py
@@ -0,0 +1,7 @@
+str_N = input("Please enter a number:")
+N = int(str_N)
+
+#N = 5
+for x in range(1,N+1):
+    print("*" * x)
+
---
+++ 2023/examples-in-class-2023-11-03/Lab02_Question05_star_pyramid_while.py
@@ -0,0 +1,9 @@
+str_N = input("Please enter a number:")
+N = int(str_N)
+
+#N = 5
+x = 1
+while x <= N: 
+    print("*" * x)
+    x = x + 1
+
