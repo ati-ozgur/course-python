b941a3ee47c8ac890333a7985cee339f497fc73b
Atilla Ozgur <ati.ozgur@gmail.com>
2023-11-03 20:13:45 +0100

    examples are added

---
+++ 2023/examples-in-class-2023-11-03/dictionary_example1.py
@@ -0,0 +1,16 @@
+# dictionary 
+# I would like to find employee phone number by their email
+
+# key
+# value
+# [] this create lists
+
+dict1 = {}
+dict1["49"] = "Germany"
+dict1["90"] = "Türkiye"
+dict1["972"] = "Israel"
+dict1["250"] = "Rwanda"
+
+print(dict1["250"])
+
+
---
+++ 2023/examples-in-class-2023-11-03/list_example1.py
@@ -0,0 +1,4 @@
+list1 = ["Atilla",1,1.0]
+
+
+print(list1)
