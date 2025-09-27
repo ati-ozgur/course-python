a013e48cf73e209e3a2e7179877d945a541baf67
Atilla Özgür <ati.ozgur@gmail.com>
2025-09-27 18:13:10 +0300

    working example, quarto error fixed

---
+++ course-content/Ornekler/Person2.py
@@ -0,0 +1,9 @@
+class Person:
+
+	def __init__(self,first_name,last_name):
+		self.first_name = first_name
+		self.last_name = last_name
+
+
+    def greeting(self):
+        return f"hello, {self.first_name} {self.last_name}"
